ba7fea2caf27b0b0362d818e90af988d60e0bc8e
Liam Powell <bsod1998@gmail.com>
2018-12-03 09:55:16 +0100

    add data persistence to Travel, changes to Person class

--- TravelMate/Model/Person.swift
+++ TravelMate/Model/Person.swift
@@ -8,17 +8,12 @@
 
 import Foundation
 
-class Person: Hashable {
+class Person {
     private var name = ""
     private var budget: Float = 0.0
-    var hashValue: Int { return ObjectIdentifier(self).hashValue }
     
     init (n: String ,b: Float){
         name = n
         budget = b
-    }
-    
-    static func ==(lhs: Person, rhs: Person) -> Bool {
-        return lhs === rhs
     }
 }
