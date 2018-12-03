5aea85ede692fcd42b86c333b4e99daa321f8bf2
Naomi Herimandimby <herimann@wificampus-118050.grenet.fr>
2018-12-03 08:48:59 +0100

    class person

---
+++ Model/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  TravelMate
+//
+//  Created by Naomi Herimandimby on 03/12/2018.
+//  Copyright © 2018 Liam Powell. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    private var name = ""
+    private var budget = 0
+    
+    public func Person (n: String ,b: Int){
+        name = n
+        budget = b
+    }
+}
