8ddb6822d4edc117c196245d23c616a7e8e5d9a4
Unknown <naohery@gmail.com>
2018-12-10 08:40:35 +0100

    ViewNao

---
+++ TravelMate/Controller/Comptes.swift
@@ -0,0 +1,21 @@
+//
+//  Comptes.swift
+//  TravelMate
+//
+//  Created by Naomi Herimandimby on 07/12/2018.
+//  Copyright © 2018 Liam Powell. All rights reserved.
+//
+
+import UIKit
+
+class Comptes: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
