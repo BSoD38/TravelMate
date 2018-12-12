c6ce2e1bd091571c80fd8837368a0953582fea68
Unknown <naohery@gmail.com>
2018-12-12 16:42:39 +0100

    ControllerNao

--- TravelMate/Controller/MyBudget.swift
+++ TravelMate/Controller/MyBudget.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class MyBudget: UIView {
 
+    @IBOutlet weak var budget: UIView!
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
