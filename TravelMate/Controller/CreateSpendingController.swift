2aa8018b86cc145822cff0845fe0109f2e726700
Liam Powell <bsod1998@gmail.com>
2018-12-12 18:44:13 +0100

    Fix things, still very glitchy

---
+++ TravelMate/Controller/CreateSpendingController.swift
@@ -0,0 +1,9 @@
+//
+//  CreateSpendingController.swift
+//  TravelMate
+//
+//  Created by Liam Powell on 12/12/2018.
+//  Copyright © 2018 Liam Powell. All rights reserved.
+//
+
+import Foundation
